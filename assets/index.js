71da8bd61ada692d352c1f83b0aaefe8af5ee35f
shriyanyk <143213476+shriyanyk@users.noreply.github.com>
2024-07-15 06:50:00 +0530

    Add files via upload

---
+++ assets/index.js
@@ -0,0 +1,19 @@
+import i1 from './i1.jpg';
+import i2 from './i2.jpg';
+import i3 from './i3.jpg';
+import i4 from './i4.jpg';
+import i5 from './i5.jpg';
+import i6 from './i6.jpg';
+import i7 from './i7.jpg';
+import i8 from './i8.png';
+import i9 from './i9.jpg';
+import i10 from './i10.jpg';
+import i11 from './i11.jpg';
+import i12 from './i12.jpg';
+import i13 from './i13.jpg';
+import i14 from './i14.jpg';
+import i15 from './i15.jpg';
+
+export{
+    i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12,i13,i14,i15
+};
